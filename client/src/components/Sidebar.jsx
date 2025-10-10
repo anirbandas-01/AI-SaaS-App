3f1de83c66dfed1d409cee6a301a5945b1fce9e2
Anirban <anirban140963@gmail.com>
2025-10-10 22:32:10 +0530

    work on sidebar and layout just add user avatar and name in sidebar and a logic that routing with login

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useUser, useClerk } from '@clerk/clerk-react';
 
-const Sidebar = (sidebar, setSidebar) => {
+const Sidebar = ({ sidebar, setSidebar }) => {
 
-    const {user} = useUser();
-    const {signOut, openUserProfile} = useClerk()
+    const { user } = useUser();
+    const { signOut, openUserProfile } = useClerk()
 
   return (
     <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
